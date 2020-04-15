bf40666014ff843439515baa3cffd81acc55c55d
Justin Cichra <1342149+jrcichra@users.noreply.github.com>
2020-04-15 15:31:06 -0400

    see what gets constructed

--- ci.sh
+++ ci.sh
@@ -16,6 +16,7 @@ kubectl get pods -A
 export PASSWORD=$(kubectl -n openfaas get secret basic-auth -o jsonpath="{.data.basic-auth-password}" | base64 --decode)
 echo -n $PASSWORD | faas-cli login --username=admin --password-stdin
 faas-cli new faas-kind-ci --lang python3 --prefix=jrcichra
+ls -lartR
 cd faas-kind-ci/faas-kind-ci 
 faas-cli up -f faas-kind-ci.yml 
 sleep 5
